2761dc602fecdd4cbc130d42f8063001b2a46e08
Daniel Pereira <Danielsilva25@live.com>
2022-10-10 14:57:57 -0300

    Add Comentarios

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, mapTo } from 'rxjs/operators';
